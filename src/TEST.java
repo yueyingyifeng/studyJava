c7eeb93475c2fa6ae96c0d2f4693f2c166e34c77
yyyf <yueyingyifeng@gmail.com>
2022-06-14 10:33:33 +0800

    ...

--- src/TEST.java
+++ src/TEST.java
@@ -1,18 +1,35 @@
 interface I{
-    int getA();
+    public int getA();
+    public void say();
 }
 
-class A{
+class A implements I{
     int a = 0;
-    int getA(){
+    @Override
+    public int getA(){
         return a;
+    }
+
+    public void say(){
+        System.out.println("abc");
     }
 }
 
 public class TEST{
 
     public static void main(String[] args){
+        I i = new I(){
+            public int getA(){
+                return 0;
 
+            }
+            public void say(){
+                System.out.println("hi");
+            }
+        };
 
+        A a = (A)i;
+        a.say();
+        
     }
 }
